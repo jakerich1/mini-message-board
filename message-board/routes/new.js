59cce0ab5c40ea8140bdee39dfb10280da88a84e
Jacob R <jakeriches1@gmail.com>
2021-08-28 09:29:58 +0100

    finished

---
+++ message-board/routes/new.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  res.send('post sent to new')
+})
+
+module.exports = router;
